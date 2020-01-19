25e242c3e07f09d70a1f4d24c193ee2053a93fba
Watchful1 <watchful@watchful.gr>
2020-01-18 18:08:31 -0800

    Missed a file

--- cowserver.py
+++ cowserver.py
@@ -11,63 +11,66 @@ import os
 import updateScripts
 
 content_security_policy = {
-    'style-src': '\'self\''
+	'style-src': '\'self\''
 }
 
-def setupApp():
-    app = Flask(__name__)
 
-    # HTTP security headers
-    #Talisman(app, content_security_policy=content_security_policy)
+def setup_app():
+	app = Flask(__name__)
 
-    # CSRF library
-    SeaSurf(app)
+	# HTTP security headers
+	# Talisman(app, content_security_policy=content_security_policy)
 
-    # Limiter
-    limiter.init_app(app)
+	# CSRF library
+	SeaSurf(app)
 
-    # SQLAlchemy
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cowserver.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	# Limiter
+	limiter.init_app(app)
 
-    db.init_app(app)
+	# SQLAlchemy
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cowserver.db'
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Blueprints
-    app.register_blueprint(redditflair)
-    app.register_blueprint(user_verification)
-    app.register_blueprint(flair_sheets)
+	db.init_app(app)
 
-    # Redis Session Interface
-    app.session_interface = RedisSessionInterface()
+	# Blueprints
+	app.register_blueprint(redditflair)
+	app.register_blueprint(user_verification)
+	app.register_blueprint(flair_sheets)
 
-    return app
-    
-def setupDatabase():
-    if not os.path.isfile('cowserver.db'):
-        with app.app_context():
-            db.create_all()
+	# Redis Session Interface
+	app.session_interface = RedisSessionInterface()
+
+	return app
 
 
-app = setupApp()
-setupDatabase()
+def setup_database():
+	if not os.path.isfile('cowserver.db'):
+		with app.app_context():
+			db.create_all()
 
-#cache buster
+
+app = setup_app()
+setup_database()
+
+
+# cache buster
 @app.context_processor
 def override_url_for():
-    return dict(url_for=dated_url_for)
+	return dict(url_for=dated_url_for)
+
 
 def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path, endpoint, filename)
-            if os.path.isfile(file_path):
-                values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+	if endpoint == 'static':
+		filename = values.get('filename', None)
+		if filename:
+			file_path = os.path.join(app.root_path, endpoint, filename)
+			if os.path.isfile(file_path):
+				values['q'] = int(os.stat(file_path).st_mtime)
+	return url_for(endpoint, **values)
 
-#updateScripts.databaseToReddit(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-    # For local HTTPS with cert & key files:
-    # app.run(host='0.0.0.0',ssl_context=('cert.pem', 'key.pem'))
+	app.run(host='0.0.0.0')
+	# For local HTTPS with cert & key files:
+	# app.run(host='0.0.0.0',ssl_context=('cert.pem', 'key.pem'))
